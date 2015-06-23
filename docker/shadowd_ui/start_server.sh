db59893f96f3c2075084aff7b468cd34e6353fbd
zit-hb <hb@zecure.org>
2015-06-23 23:32:21 +0200

    Cleaned up docker file.

--- docker/shadowd_ui/start_server.sh
+++ docker/shadowd_ui/start_server.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# Substitute secret token when the script is started for the first time.
 sed -i "s/ThisTokenIsNotSoSecretChangeIt/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/g" /var/shadowd_ui/app/config/parameters.yml
+
+# Start the web server.
 /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -D
