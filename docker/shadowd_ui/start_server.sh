dd4aa45e50c7a82f156c5f688b5c3f38e0bbd11c
zit-hb <hb@zecure.org>
2018-01-28 18:13:08 +0100

    Use postgres based database

--- docker/shadowd_ui/start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Substitute secret token when the script is started for the first time.
-sed -i "s/ThisTokenIsNotSoSecretChangeIt/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 32)/g" /var/shadowd_ui/app/config/parameters.yml
-
-# Start the web server.
-/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -D
